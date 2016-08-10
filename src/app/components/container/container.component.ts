b1af1e569055113e8394ac5252ab84be3fdd595b
mikkeldamm <mikkeldamm@hotmail.com>
2016-08-11 01:41:27 +0200

    container component for frame added

---
+++ src/app/components/container/container.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cbs-container',
+    styles: [ require('./container.scss') ],
+    template: require('./container.html')
+})
+export class Container {
+
+    constructor() {
+
+    }
+}
