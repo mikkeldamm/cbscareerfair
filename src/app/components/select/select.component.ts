4f39b5407582ab764cfa8da7185f57c7add23d8a
mikkeldamm <mikkeldamm@hotmail.com>
2016-08-11 01:40:48 +0200

    select style from css to scss

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'cbs-select',
-    styles: [ require('./select.css') ],
+    styles: [ require('./select.scss') ],
     template: require('./select.html')
 })
 export class Select {
