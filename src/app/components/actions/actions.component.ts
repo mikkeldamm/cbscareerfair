3ee132f78284d55c56d86277297752b962a68888
Mikkel Richter Damm <mrd@one.com>
2016-08-11 14:21:34 +0200

    style for cbs-actions

--- src/app/components/actions/actions.component.ts
+++ src/app/components/actions/actions.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'cbs-actions',
     styles: [ require('./actions.scss') ],
     template: require('./actions.html')
 })
-export class Actions {
+export class Actions implements OnInit {
+
+    position: string;
+    profile: string;
 
     constructor() {
 
