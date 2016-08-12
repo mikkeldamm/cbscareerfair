10c9d050ca021257f01cd160da7e33ec5517a8c2
Mikkel Richter Damm <mrd@one.com>
2016-08-12 15:16:16 +0200

    changed port and removed env. variables

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,9 +3,9 @@ const webpackMerge = require('webpack-merge'); // used to merge webpack configs
 const commonConfig = require('./webpack.common.js'); // the settings that are common to prod and dev
 const DefinePlugin = require('webpack/lib/DefinePlugin');
 
-const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+const ENV = 'development';
+const HOST = 'localhost';
+const PORT = 5555;
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = webpackMerge(commonConfig.metadata, {
     host: HOST,
