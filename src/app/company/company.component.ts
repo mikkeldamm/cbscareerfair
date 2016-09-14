22a352fc23ede69e955a807a4166601ba3af83a0
mikkeldamm <mikkeldamm@hotmail.com>
2016-09-14 19:44:10 +0200

    setup route params on company page

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Subscription } from 'rxjs/Subscription';
 
 import { StateService } from '../store';
 
 @Component({
-  selector: 'company',
-  styleUrls: [ './company.style.scss' ],
-  templateUrl: './company.template.html'
+    selector: 'company',
+    styleUrls: ['./company.style.scss'],
+    templateUrl: './company.template.html'
 })
 export class Company {
 
-  constructor(public _state: StateService) {
+    private _sub: Subscription;
 
-  }
+    constructor(private _route: ActivatedRoute, private _state: StateService) {
+
+    }
+
+    ngOnInit() {
+
+        this._sub = this._route.params.subscribe(params => {
+            console.log(params);
+        });
+    }
+
+    ngOnDestroy() {
+        
+        this._sub.unsubscribe();
+    }
 }
